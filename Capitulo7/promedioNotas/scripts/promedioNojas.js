3dd958601766d6d01149bbca3b0af5784612aa70
alejandro1223342 <alejomuoss@gmail.com>
2024-08-10 22:53:41 -0500

    Funcion para mostrar imagenes implementada

--- Capitulo7/promedioNotas/scripts/promedioNojas.js
+++ Capitulo7/promedioNotas/scripts/promedioNojas.js
@@ -12,7 +12,11 @@ calcularPromedioNotas = function () {
     promedio=calcularPromedio(cmpNota1,cmpNota2,cmpNota3);
     mostrartexto("lblResultado",promedio.toFixed(2));
 
-    
+    if (promedio > 7){
+        mostrarImagen("imgResultado","../files/img/exito.gif")
+    }else{
+        mostrarImagen("imgResultado","../files/img/fallo.gif")
+    }
     return promedio;
 
 
