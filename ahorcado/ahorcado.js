0aa1acc64630acd867acc320dccf5eba2cb002bd
alejandro1223342 <alejomuoss@gmail.com>
2024-08-18 21:09:08 -0500

    Se crea la funcion esMayuscula

---
+++ ahorcado/ahorcado.js
@@ -0,0 +1,11 @@
+//No se olvide de respirar, mantenga la calma y demuestre lo que sabe
+
+
+esMayuscula = function (caracter){
+
+    if (caracter.charCodeAt(0) >= 65 && caracter.charCodeAt(0) <= 90) {
+        return true;
+    } else {
+        return false;
+    }
+}
